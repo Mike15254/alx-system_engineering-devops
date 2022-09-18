6a713ca33945b8040400fe87732776f71a2af872
Mike15254 <mike8max10@gmail.com>
2022-09-18 21:33:34 +0300

    update commit

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,6 @@
 # Creates a file in /tmp
-
-file { '/tmp/holberton':
-  ensure  => file,
-  path    => '/tmp/holberton',
+file { '/tmp/school':
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
